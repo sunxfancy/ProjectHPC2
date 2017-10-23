52de6062408d2402927e0b5aa0389ca2ec8a3660
Xiaofan <xiaofan@sabayon.local>
2017-10-22 19:40:07 -0700

    finished

--- Project2/src/common.h
+++ Project2/src/common.h
@@ -3,25 +3,28 @@
 #include <time.h>
 
 double*
-createMatrix(unsigned int n) {
-    return (double*) calloc(sizeof(double), n*n+1);
+createMatrix(unsigned int n, unsigned int m) {
+    return (double*) calloc(sizeof(double), n*m+1);
 }
 
 double*
-createMatrixWithRandomData(unsigned int n) {
+createMatrixWithRandomData(unsigned int n, unsigned int m) {
     double* data; unsigned int i;
-    data = createMatrix(n);
-    for (i = 0; i < n*n; ++i) {
+    data = createMatrix(n, m);
+    for (i = 0; i < n*m; ++i) {
         data[i] = (double)(rand() % 9987) / 100.0;
     }
     return data;
 }
 
 int
-checkEqual(double *a, double *b, unsigned int n) {
+checkEqual(double *a, double *b, unsigned int n, unsigned int m) {
     unsigned int i;
-    for (i = 0; i< n; ++i) {
-        if (a[i] != b[i]) return 0;
+    for (i = 0; i< n*m; ++i) {
+        if (a[i] != b[i]) {
+            printf("index: i = %d, j = %d\n", i / m, i % m);
+            return 0;
+        }
     }
     return 1;
 }
@@ -39,23 +42,27 @@ void timespec_diff(struct timespec *start, struct timespec *stop,
     return;
 }
 
-void printMatrix(double* m, unsigned int n) {
+void printMatrix(double* c, unsigned int n, unsigned int m, unsigned int lc) {
     unsigned int i, j;
     for (i = 0; i < n; ++i) {
-        for (j = 0; j < n; ++j)
-            printf("%.2f ", m[i*n+j]);
+        for (j = 0; j < m; ++j)
+            printf("%.2f ", c[i*lc+j]);
         printf("\n");
     }
 }
 
-typedef void (*matrixMutiply)(double *a, double *b, double *c, unsigned int n);
+inline void printMatrix(double* c, unsigned int n, unsigned int m) {
+    printMatrix(c, n, m, m);
+}
 
-double* runTest(matrixMutiply func, double *a, double *b, unsigned int n) {
+typedef void (*matrixMutiply)(double *a, double *b, double *c, unsigned int n, unsigned int n1, unsigned int n2);
+
+double* runTest(matrixMutiply func, double *a, double *b, unsigned int n, unsigned int n1, unsigned int n2) {
     double *c, timed, n3, p;
     struct timespec ts1,ts2,diff;
-    c = createMatrix(n);
+    c = createMatrix(n, n2);
     clock_gettime(CLOCK_REALTIME, &ts1);
-    func(a,b,c,n);
+    func(a,b,c,n,n1,n2);
     clock_gettime(CLOCK_REALTIME, &ts2);
     timespec_diff(&ts1,&ts2,&diff);
     timed = (double)(diff.tv_sec) + 1e-9 * (double)(diff.tv_nsec);
@@ -64,4 +71,147 @@ double* runTest(matrixMutiply func, double *a, double *b, unsigned int n) {
     printf("performace: %lf Gflops\n", p);
     printf("timespec is %lu s %lu ns\n", diff.tv_sec, diff.tv_nsec);
     return c;
+}
+
+void
+dgemm_ikj(double *a, double *b, double *c, unsigned int n, unsigned int n1, unsigned int n2) {
+    unsigned int i,j,k;
+    for (i=0; i<n; i++)
+        for (k=0; k<n1; k++) {
+            register double r = a[i*n1+k];
+            for (j=0; j<n2; j++)
+                c[i*n2+j] += r * b[k*n2+j];
+        }
+}
+
+
+void
+dgemm_mixed(double *a, double *b, double *c, unsigned int n, unsigned int n1, unsigned int n2, unsigned int la, unsigned int lb, unsigned int lc) {
+    const unsigned int B = 30;
+    unsigned int i, j, k, i1, j1, k1, m, m1, m2, r, p;
+    /* ikj – blocked version algorithm*/
+    m = n/B*B; m1 = n1/B*B; m2 = n2/B*B;
+    p = (B/3)*3;
+    r = B - 2;
+    for (i = 0; i < m; i+=B)
+        for (k = 0; k < m1; k+=B) 
+            for (j = 0; j < m2; j+=B) 
+                /* B x B mini matrix multiplications */
+                {
+                    for (i1 = i; i1 < i+r; i1+=3)
+                        for (j1 = j; j1 < j+r; j1+=3) {
+                            unsigned int t = i1*lc+j1; unsigned int t1 = t+lc; unsigned int t2 = t+2*lc;
+                            register double c00 = c[t];  register double c01 = c[t+1];  register double c02 = c[t+2];
+                            register double c10 = c[t1]; register double c11 = c[t1+1]; register double c12 = c[t1+2];
+                            register double c20 = c[t2]; register double c21 = c[t2+1]; register double c22 = c[t2+2];
+                            for (k1 = k; k1 < k+r; k1+=3) {
+                                unsigned int ta = i1*la+k1; unsigned int ta1 = ta+la; unsigned int ta2 = ta+2*la;
+                                unsigned int tb = k1*lb+j1; unsigned int tb1 = tb+lb; unsigned int tb2 = tb+2*lb;
+                
+                                register double a00 = a[ta]; register double a10 = a[ta1];  register double a20 = a[ta2];
+                                register double b00 = b[tb]; register double b01 = b[tb+1]; register double b02 = b[tb+2];
+                                c00 += a00*b00; c01 += a00*b01; c02 += a00*b02;
+                                c10 += a10*b00; c11 += a10*b01; c12 += a10*b02;
+                                c20 += a20*b00; c21 += a20*b01; c22 += a20*b02;
+                
+                                a00 = a[ta+1]; a10 = a[ta1+1]; a20 = a[ta2+1];
+                                b00 = b[tb1];  b01 = b[tb1+1]; b02 = b[tb1+2];
+                                c00 += a00*b00; c01 += a00*b01; c02 += a00*b02;
+                                c10 += a10*b00; c11 += a10*b01; c12 += a10*b02;
+                                c20 += a20*b00; c21 += a20*b01; c22 += a20*b02;
+                
+                                a00 = a[ta+2]; a10 = a[ta1+2]; a20 = a[ta2+2];
+                                b00 = b[tb2];  b01 = b[tb2+1]; b02 = b[tb2+2];
+                                c00 += a00*b00; c01 += a00*b01; c02 += a00*b02;
+                                c10 += a10*b00; c11 += a10*b01; c12 += a10*b02;
+                                c20 += a20*b00; c21 += a20*b01; c22 += a20*b02;
+                            }
+                
+                            for (k1 = k+p; k1 < k+B; ++k1) {
+                                unsigned int ta = i1*la+k1; unsigned int ta1 = ta+la; unsigned int ta2 = ta+2*la;
+                                unsigned int tb = k1*lb+j1;
+                                register double a00 = a[ta]; register double a10 = a[ta1];  register double a20 = a[ta2];
+                                register double b00 = b[tb]; register double b01 = b[tb+1]; register double b02 = b[tb+2];
+                                c00 += a00*b00; c01 += a00*b01; c02 += a00*b02;
+                                c10 += a10*b00; c11 += a10*b01; c12 += a10*b02;
+                                c20 += a20*b00; c21 += a20*b01; c22 += a20*b02;
+                            }
+                
+                            c[t]  = c00; c[t+1]  = c01; c[t+2]  = c02;
+                            c[t1] = c10; c[t1+1] = c11; c[t1+2] = c12;
+                            c[t2] = c20; c[t2+1] = c21; c[t2+2] = c22;
+                        }
+            
+                    for (i1 = i; i1 < i+B; ++i1)
+                        for (j1 = j+p; j1 < j+B; ++j1)
+                            for (k1 = k; k1 < k+B; ++k1)
+                                c[i1*lc+j1] += a[i1*la+k1] * b[k1*lb+j1];
+                    for (i1 = i+p; i1 < i+B; ++i1)
+                        for (j1 = j; j1 < j+p; ++j1)
+                            for (k1 = k; k1 < k+B; ++k1)
+                                c[i1*lc+j1] += a[i1*la+k1] * b[k1*lb+j1];
+                }
+    // A01 x B10
+    for (i = 0; i < m; i+=B)
+        for (k = m1; k < n1; ++k) 
+            for (j = 0; j < m2; j+=B)  
+                /* B x B mini matrix multiplications */
+                for (i1 = i; i1 < i+B; ++i1) {
+                    register double r=a[i1 * n1 + k];
+                    for (j1 = j; j1 < j+B; ++j1) 
+                        c[i1 * lc + j1] += r * b[k * lb + j1];
+                }
+    for (i = 0; i < m; ++i) 
+        for (k = 0; k < n1; ++k)
+            for (j = m2; j< n2; ++j)
+                c[i * lc + j] += a[i * la + k] * b[k * lb + j]; 
+    for (i = m; i < n; ++i)
+        for (k = 0; k < n1; ++k)
+            for (j = 0; j< n2; ++j)
+                c[i * lc + j] += a[i * la + k] * b[k * lb + j]; 
+}
+
+inline void
+dgemm_mixed(double *a, double *b, double *c, unsigned int n, unsigned int n1, unsigned int n2) {
+    dgemm_mixed(a,b,c,n,n1,n2,n1,n2,n2);
+}
+
+void
+copy_matrix(double *a, double *b, unsigned int n, unsigned int m, unsigned int la, unsigned int lb) {
+    for (int i = 0; i < n; ++i) 
+        for (int j = 0; j < m; ++j)
+            a[i*la+j] = b[i*lb+j];
+}
+
+void
+minus_matrix(double *a, double *b, unsigned int n, unsigned int m, unsigned int la, unsigned int lb) {
+    for (int i = 0; i < n; ++i) 
+        for (int j = 0; j < m; ++j)
+            a[i*la+j] -= b[i*lb+j];
+}
+
+inline void
+copy_matrix(double *a, double *b, unsigned int n, unsigned int m) {
+    copy_matrix(a, b, n, m, m, m);
+}
+
+bool dgemm_mixed_test(unsigned int n, unsigned int n1, unsigned int n2) {
+    printf("------------------------\n");
+    printf("n: %d, %d, %d\n", n, n1, n2);
+
+    double* a = createMatrixWithRandomData(n, n1);
+    double* b = createMatrixWithRandomData(n1, n2);
+
+    double* c0 = runTest(dgemm_ikj, a, b, n, n1, n2);
+    double* c = runTest(dgemm_mixed, a, b, n, n1, n2);
+    if (checkEqual(c0, c, n, n2) == 0) {
+        printf("%s output error\n", "dgemm_mixed");
+        printMatrix(c0, n, n2);
+        printMatrix(c, n, n2);
+        exit(1);
+    } else {
+        printf("%s output checked\n", "dgemm_mixed");
+    }
+    free(a);  free(b);
+    free(c0); free(c);
 }
--- Project2/src/main.cpp
+++ Project2/src/main.cpp
@@ -3,6 +3,7 @@
 #include "cblas.h"
 #include <cstring>
 #include <cmath>
+#include "common.h"
 
 void print_matrix(const char* desc, lapack_int m, lapack_int n, double* a, lapack_int lda ) {
     lapack_int i, j;
@@ -87,11 +88,121 @@ double* mydtrsm(dtrsm_type t, int n, double* a, double* b, int* pvt) {
     }
 }
 
+void inv_triangle(int n, double* a) {
+    for (int i = 1; i < n; ++i)
+        A(i,0) = -A(i,0);
+
+    for (int k=1; k<n-1; ++k)
+        for(int i = k+1; i < n; ++i) {
+            for(int j = 0; j < k; j++)
+               A(i,j) -= A(k,j) * A(i,k);
+            A(i,k) = -A(i,k);
+        }
+} 
+
+
+#define LL(x, y) ll[(x)*B + (y)]
+
+int hp_dgetrf(int n, double* a, int* pvt) { 
+    for (int i = 0; i < n-1; ++i) {
+        int maxind = i;
+        double maxa = fabs(A(i, i)); 
+        for (int t = i + 1; t < n; ++t) {
+            if (fabs(A(t, i)) > maxa) {
+                 maxa = fabs(A(t, i));
+                 maxind = t;
+            }
+        }
+        if (maxa == 0.0) {
+            return -1;
+        } else {
+            for (int j = 0; j<n; ++j) pvt[j] = j;
+            if (maxind != i) {
+                int temps = pvt[i];
+                pvt[i] = pvt[maxind];
+                pvt[maxind] = temps;
+
+                for (int j = 0; j < n; ++j) {
+                    temps = A(i, j);
+                    A(i, j) = A(maxind, j);
+                    A(maxind, j) = temps;
+                }
+            }
+        }
+    }
+    const int B = 30;
+    int m = n/B*B; int m1 = (n-1)/B*B; 
+    double* ll = createMatrix(B, B);
+    double* temp = createMatrix(n, n);
+    int i;
+    for (i = 0; i < m1; i += B) {
+        int end = i+B;
+        // apply BLAS2 version to get A(i:n, i:i+B)
+        for (int t = i; t < end; ++t)
+            for (int j = t+1; j < n; ++j) {
+                A(j, t) /= A(t, t);
+                for (int k = t+1; k < t+B; ++k) {
+                    A(j, k) -= A(j, t) * A(t, k);
+                }
+            }
+        // printf("-------------------\n");
+        // printMatrix(&A(i, i), 3, 3, n);
+        
+        // get LL
+        for (int p = 0; p < B; ++p)
+            for (int q = 0; q < B; ++q)
+                if (p == q) LL(p, q) = 1;
+                else if (p < q) LL(p, q) = 0;
+                else LL(p, q) = A(i+p, i+q);
+        // printf("-------------------\n");
+        // printMatrix(ll, B, B, B);
+        inv_triangle(B, ll); // LL^-1
+        // printf("-------------------\n");
+        // printMatrix(ll, B, B, B);
+
+        dgemm_mixed(ll, &A(i, end), temp, B, B, n-end, B, n, n-end);   // LL-1 * A(ib:end , end+1:n)
+        // printf("-------------------\n");
+        // printMatrix(temp, B, n-end, n-end);
+        copy_matrix(&A(i, end), temp, B, n-end, n, n-end);  // update A(ib:end , end+1:n)
+        
+        // printf("-------------------\n");
+        // printMatrix(&A(i, i), 3, 3, n);
+
+        dgemm_mixed(&A(end, i), &A(i, end), temp, n-end, B, n-end, n, n, n-end);
+        minus_matrix(&A(end, end), temp, n-end, n-end, n, n-end);
+    }
+    for (; i < n-1; ++i) { // continue to do the unfinished part
+        for (int j = i+1; j < n; ++j) {
+            A(j, i) /= A(i, i);
+            for (int k = i+1; k < n; ++k) {
+                A(j, k) -= A(j, i) * A(i, k);
+            }
+        }
+    }
+    free(ll); free(temp);
+    return 0;
+}
+
+
 
 #undef A
 
  
 int main() {
+    dgemm_mixed_test(3, 4, 5);
+    dgemm_mixed_test(30, 40, 50);
+    dgemm_mixed_test(300, 600, 300);
+    dgemm_mixed_test(308, 600, 38);
+
+    double l[] = {
+        1,0,0,0,
+        2,1,0,0,
+        3,2,1,0,
+        5,4,2,1
+    };
+    inv_triangle(4, l);
+    printMatrix(l, 4, 4);
+
     const int m = 1, n = 3;
     printf("lapack test\n");
     double a[] = 
@@ -101,14 +212,17 @@ int main() {
 		-3,2,1
     };
     
-    double aa[9];
+    double aa[9], aaa[9];
     memcpy(aa, a, sizeof(double)*9);
-    int ipiv[3], iipiv[3];
+    memcpy(aaa, a, sizeof(double)*9);
+    int ipiv[3], iipiv[3], iiipiv[3];
     print_matrix("a", n, n, a, n);
     
     mydgetrf(3, aa, iipiv);
     print_matrix("LU of a by myself", n, aa);
 
+    hp_dgetrf(3, aaa, iiipiv);
+    print_matrix("LU of hp_dgetrf", n, aaa);
 
     lapack_int info = LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, a, n, ipiv); 
     print_matrix("LU of a", n, n, a, n); 
@@ -117,7 +231,9 @@ int main() {
     
     double b[] = {1, 1, 0};
     double bb[3];
+    double bbb[3];
     memcpy(bb, b, sizeof(double)*3);
+    memcpy(bbb, b, sizeof(double)*3);
     print_matrix("b", 1, 3, b, n);
     
 
@@ -125,6 +241,12 @@ int main() {
     double* x = mydtrsm(Upper, 3, aa, y, iipiv);
     print_matrix("my", 1, 3, y, 3);
     print_matrix("mx", 1, 3, x, 3);
+
+
+    double* yy = mydtrsm(Lower, 3, aaa, bbb, iiipiv);
+    double* xx = mydtrsm(Upper, 3, aaa, yy, iiipiv);
+    print_matrix("myy", 1, 3, yy, 3);
+    print_matrix("mxx", 1, 3, xx, 3);
 
     for(int i = 0; i < n; i++)
     {
